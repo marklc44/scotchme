977700024d613f7fe471722fac3b2e6d54f579da
Mark Centoni <marklc44@gmail.com>
2014-08-04 21:28:01 -0700

    complete pretty sweet styling of the search form

---
+++ public/js/search.js
@@ -0,0 +1,29 @@
+var $dSearchInputs = $('.flavor-module').find('input[type="text"]');
+var $dialBoxes = $('.dial-box');
+
+var highlightDials = function($input, value) {
+	$input.siblings('.dial-wrapper').find('.dial-box')
+		.each(function() {
+			console.log($(this));
+			var num = $(this).attr('data-position');
+			if(num <= value) {
+				$(this).addClass('highlight');
+			} else {
+				$(this).removeClass('highlight');
+			}
+		});
+};
+
+$dSearchInputs.each(function() {
+	$(this).keyup(function() {
+		var value = $(this).val();
+		highlightDials($(this), value);
+	});
+});
+
+$dialBoxes.click(function() {
+	var $thisInput = $(this).parent().siblings('input[type="text"]');
+	var thisValue = $(this).attr('data-position');
+	$thisInput.val(thisValue);
+	highlightDials($thisInput, thisValue);
+});
